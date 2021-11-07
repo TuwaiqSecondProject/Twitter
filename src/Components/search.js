39eedcb83af294d966cf00245d1bd1cff1ea3d65
FahadTariqiDev <fahad.tariqi17@gmail.com>
2021-11-07 13:27:23 +0300

    styling

--- src/Components/search.js
+++ src/Components/search.js
@@ -1,5 +1,10 @@
-
 const Search = () => {
-  
+  return (
+    <>
+      <div >
+        <input type="text" placeholder="Search.."  className="search-input"/>
+      </div>
+    </>
+  );
 };
 export default Search;
