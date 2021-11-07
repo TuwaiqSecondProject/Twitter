2188558c8d84895b752d9669d316b32caaa160d0
Reema <reemaibrahim@MacBook-Pro-alkhas-b-Reema.local>
2021-11-07 15:08:25 +0300

    add data

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -3,14 +3,14 @@ export const Tweets = [
     Content: "hello world",
     UserId: 1,
     tweetID: 1,
-    date: "6/novmber",
+    date: "6/nov",
     numberOfLikes: 5,
   },
   {
     Content: "BIG NEWS lol jk still Twitter",
     UserId: 2,
     tweetID: 2,
-    date: "4/novmber",
+    date: "4/nov",
     numberOfLikes: 4,
   },
   {
@@ -18,7 +18,7 @@ export const Tweets = [
       "We don’t beg for followers on twitter. You get em when you get em",
     UserId: 1,
     tweetID: 3,
-    date: "2/novmber",
+    date: "2/nov",
     numberOfLikes: 51,
   },
   {
@@ -26,7 +26,7 @@ export const Tweets = [
       "hello to everyone reading this while you should be doing something else",
     UserId: 2,
     tweetID: 4,
-    date: "6/novmber",
+    date: "6/nov",
     numberOfLikes: 2,
   },
   {
@@ -34,7 +34,7 @@ export const Tweets = [
     // img: "",
     UserId: 3,
     tweetID: 5,
-    date: "1/novmber",
+    date: "1/nov",
     numberOfLikes: 126485,
   },
   {
@@ -53,6 +53,105 @@ export const Tweets = [
     date: "19/oct",
     numberOfLikes: 4562,
   },
+  {
+    Content: "Don’t call it dream, call it a plan",
+    UserId: 1,
+    tweetID:8 ,
+    date: "10/oct",
+    numberOfLikes: 13 ,
+  },
+  {
+    Content: "Happy nice weekend ",
+    UserId: 1 ,
+    tweetID:9 ,
+    date: "14/mar",
+    numberOfLikes:765 ,
+  },
+  {
+    Content: "My life is my message.",
+    UserId: 2,
+    tweetID: 10,
+    date: "7/feb",
+    numberOfLikes: 66,
+  },
+  {
+    Content: "Yesterday you said tomorrow. Just do it",
+    UserId: 2,
+    tweetID: 11,
+    date: "23/jan",
+    numberOfLikes: 5,
+  },
+  {
+    Content: "Change the world by being yourself",
+    UserId: 3,
+    tweetID:12 ,
+    date: "10/may",
+    numberOfLikes: 89,
+  },
+  {
+    Content: "Die with memories, not dreams",
+    UserId:3 ,
+    tweetID:13 ,
+    date: "1/oct",
+    numberOfLikes: 1234,
+  },
+  {
+    Content: "Be so good they can’t ignore you",
+    UserId:3 ,
+    tweetID: 14,
+    date: "3/jun",
+    numberOfLikes: 654,
+  },
+  {
+    Content: "Silence is an answer too",
+    UserId: 4,
+    tweetID: 15,
+    date: "17/aug",
+    numberOfLikes: 87,
+  },
+  {
+    Content: "No pain No gain",
+    UserId:4 ,
+    tweetID: 16,
+    date: "18/aug",
+    numberOfLikes: 334,
+  },
+  {
+    Content: "Take the risk or lose the chance",
+    UserId: 5,
+    tweetID: 17,
+    date: "5/jul",
+    numberOfLikes: 55,
+  },
+  {
+    Content: "No pressure, no diamonds",
+    UserId: 6,
+    tweetID:18 ,
+    date: "13/dec",
+    numberOfLikes: 98,
+  },
+  {
+    Content: "What we think, we become",
+    UserId:6 ,
+    tweetID: 19,
+    date: "8/dec",
+    numberOfLikes: 87,
+  },
+  {
+    Content: "Once you choose hope, anything’s possible",
+    UserId: 7,
+    tweetID: 20,
+    date: "1/jan",
+    numberOfLikes:100 ,
+  },
+  {
+    Content: "Do not waste your time",
+    UserId: 7,
+    tweetID: 21,
+    date: "20/sep",
+    numberOfLikes: 6743,
+  },
+
 ];
 
 
@@ -87,6 +186,18 @@ export const users = [
     img:"",
     userPassword: "553320",
     userId: 5
+  },
+  { 
+    username: "Ibrahim",
+    img:"",
+    userPassword: "112233",
+    userId: 6
+  },
+  { 
+    username: "Hammad",
+    img:"",
+    userPassword: "121212",
+    userId: 7
   },
 ];
 
