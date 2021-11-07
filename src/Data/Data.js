ad583386da5b62c76636efb930aef5d28711c9b0
FahadTariqiDev <fahad.tariqi17@gmail.com>
2021-11-07 09:37:22 +0300

    update data

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -2,14 +2,14 @@ export const Tweets = [
   {
     Content: "hello world",
     UserId: 1,
-    id: 1,
+    tweetID: 1,
     date: "6/novmber",
     numberOfLikes: 5,
   },
   {
     Content: "BIG NEWS lol jk still Twitter",
     UserId: 2,
-    id: 2,
+    tweetID: 2,
     date: "4/novmber",
     numberOfLikes: 4,
   },
@@ -17,7 +17,7 @@ export const Tweets = [
     Content:
       "We don’t beg for followers on twitter. You get em when you get em",
     UserId: 1,
-    id: 3,
+    tweetID: 3,
     date: "2/novmber",
     numberOfLikes: 51,
   },
@@ -25,7 +25,7 @@ export const Tweets = [
     Content:
       "hello to everyone reading this while you should be doing something else",
     UserId: 2,
-    id: 4,
+    tweetID: 4,
     date: "6/novmber",
     numberOfLikes: 2,
   },
@@ -33,7 +33,7 @@ export const Tweets = [
     Content: "𝐁𝐈𝐆 𝐁𝐄𝐍𝐙 ",
     img: "",
     UserId: 3,
-    id: 5,
+    tweetID: 5,
     date: "1/novmber",
     numberOfLikes: 126485,
   },
@@ -41,7 +41,7 @@ export const Tweets = [
     Content:
       "I asked Angelina Jolie to sign on my arm for a tattoo and this is how it went ",
     UserId: 4,
-    id: 6,
+    tweetID: 6,
     date: "30/oct",
     numberOfLikes: 8820,
   },
@@ -49,9 +49,13 @@ export const Tweets = [
     Content:
       "I love how Kit Harrington finished GoT and just came over to the MCU to play a more funny, charismatic, badass version of Jon Snow. #Eternals",
     UserId: 4,
-    id: 7,
+    tweetID: 7,
     date: "19/oct",
     numberOfLikes: 4562,
   },
-  
+];
+
+export const users = [
+  { username: "Twitter", userPassword: "123456", userId: 2 },
+  { username: "Twitter", userPassword: "123456", userId: 1 },
 ];
