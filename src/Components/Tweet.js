c7a2a09961b16ce7a727c2a4f380c6693cc1aa73
FahadTariqiDev <fahad.tariqi17@gmail.com>
2021-11-07 15:27:29 +0300

    more edit

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -1,20 +1,31 @@
+import avatar from "../images/avatar.jpg";
+import replay from "../images/arrow-svgrepo-com.svg";
+import likes from "../images/like-svgrepo-com.svg";
+
 function Tweet(UserId, Content, date, numberOfLikes) {
   console.log(UserId);
   return (
     <>
-
       <div className="post-container">
         <div className="post">
           <div className="header">
             <div>
-              <p>{UserId.date}</p>
+              <img src={avatar} />
             </div>
             <div>
-              <p>user name: {UserId.UserId}</p>
+              <p>@twitter</p>
             </div>
           </div>
           <div className="contetnt">
             <p>{UserId.Content}</p>
+          </div>
+          <div className="footer">
+            <div>
+              <button><img src={replay}/></button>
+              
+              <button><img src={likes}/></button>
+              {/* <button> like</button> */}
+            </div>
           </div>
         </div>
       </div>
