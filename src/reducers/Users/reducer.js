bd88d9bf27ce02e3228d47a03b2b5bc9f973315c
Reema <reemaibrahim@MacBook-Pro-alkhas-b-Reema.local>
2021-11-09 13:21:04 +0300

    avatar,header

--- src/reducers/Users/reducer.js
+++ src/reducers/Users/reducer.js
@@ -5,34 +5,40 @@ const initialState = {
       img: "",
       userPassword: "123456",
       userId: 2,
-      img: "",
-      header: "",
+      img: "./images/avatar5.jpg",
+      header: "./images/header6.jfif",
     },
     {
       username: "Noura",
       img: "",
       userPassword: "654321",
       userId: 1,
-      img: "./images/avatar2.jpg",
-      header: "./images/header1.jfif",
+      img: "./images/avatar3.jpg",
+      header: "./images/header2.jfif",
     },
     {
       username: "Fahad",
       img: "",
       userPassword: "654320",
       userId: 3,
+      img: "./images/avatar6.jpg",
+      header: "./images/header5.jfif",
     },
     {
       username: "Amal",
       img: "",
       userPassword: "653320",
       userId: 4,
+      img: "./images/avatar2.jpg",
+      header: "./images/header1.jfif",
     },
     {
       username: "Rema",
       img: "",
       userPassword: "553320",
       userId: 5,
+      img: "./images/avatar4.jpg",
+      header: "./images/header5.jfif",
     },
   ],
 };
@@ -49,3 +55,16 @@ const usersReducer = (state = initialState, { type, payload }) => {
   }
 };
 export default usersReducer;
+
+
+// user: ibrahim 
+
+// img: "./images/avatar7.jpg",
+//       header: "./images/header6.jfif",
+
+
+//       user: hammad
+
+//       img: "./images/avatar8.jpg",
+//       header: "./images/header7.jfif",
+
